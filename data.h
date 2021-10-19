f911132d530688042fdff10552133bf1da9c468d
Clélia de Mulatier <clelidm@gmail.com>
2021-10-19 16:05:42 +0200

    Add files via upload

--- data.h
+++ data.h
@@ -5,12 +5,15 @@ using namespace std;
 /********************************************************************/
 /**************************    CONSTANTS    *************************/
 /********************************************************************/
-    const unsigned int n = 9;                          // number of spin variables
+
+// number of binary (spin) variables:
+const unsigned int n = 9; 
 
 
-    const string datafilename = "INPUT/Dataset_Shapes_n9_N1e5.dat";			// Input datafile
-    const string basis_IntegerRepresentation_filename = "INPUT/Dataset_Shapes_n9_Basis_Integer.dat";	// (optional) Input basis file
-    const string basis_BinaryRepresentation_filename = "INPUT/Dataset_Shapes_n9_Basis_Binary.dat";		// (optional) Input basis file
+// (optional) INPUT FILES:
+const string datafilename = "INPUT/Dataset_Shapes_n9_N1e5.dat";   // Input datafiles
+const string basis_IntegerRepresentation_filename = "INPUT/Dataset_Shapes_n9_Basis_Integer.dat";	// (optional) Input basis file
+const string basis_BinaryRepresentation_filename = "INPUT/Dataset_Shapes_n9_Basis_Binary.dat";		// (optional) Input basis file
 
-
-    const string OUTPUT_directory = "OUTPUT/";
+// OUTPUT FOLDER:
+const string OUTPUT_directory = "OUTPUT/";
