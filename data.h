094ee9e5dd8a4ae040f5835a7b5dc3269e5212b7
Clélia de Mulatier <clelidm@gmail.com>
2024-06-07 17:15:22 +0200

    Add files via upload

--- data.h
+++ data.h
@@ -11,9 +11,12 @@ const unsigned int n = 9;
 
 
 // (optional) INPUT FILES:
-const string datafilename = "INPUT/Shapes_n9_Dataset_N1e5.dat";   // Input datafiles
+const string datafilename =  "INPUT/SCOTUS_n9_N895_Data.dat";  // "INPUT/Shapes_n9_Dataset_N1e5.dat";  // Input datafiles
+const string basis_BinaryRepresentation_filename = "INPUT/SCOTUS_n9_BestBasis_Binary.dat"; //"INPUT/Shapes_n9_BestBasis_Binary.dat";		// (optional) Input basis file
+
+
 const string basis_IntegerRepresentation_filename = "INPUT/Shapes_n9_BestBasis_Integer.dat";	// (optional) Input basis file
-const string basis_BinaryRepresentation_filename = "INPUT/Shapes_n9_BestBasis_Binary.dat";		// (optional) Input basis file
+
 
 // OUTPUT FOLDER:
 const string OUTPUT_directory = "OUTPUT/";
